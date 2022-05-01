48406e262f6db8d3d0f234b89780475de4b0e26e
Jesus Montano <jesusmontano96@gmail.com>
2022-05-01 11:49:27 -0700

    feat: adds registration page [WT-1]
    Adds field validations and integrates the `/register` endpoint to the registration page.

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import {
+  CAlert,
   CButton,
   CCard,
   CCardBody,
   CCol,
   CContainer,
   CForm,
+  CFormFeedback,
   CFormInput,
   CInputGroup,
   CInputGroupText,
   CRow,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import { cilLockLocked, cilUser } from '@coreui/icons'
+import { cilLockLocked } from '@coreui/icons'
+const validator = require('validator')
+const axios = require('axios').default
 
 const Register = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isAlertVisible, setIsAlertVisible] = useState(false)
+  const [email, setEmail] = useState('')
+  const [isEmailDirty, setIsEmailDirty] = useState(false)
+  const [password, setPassword] = useState('')
+  const [isPasswordDirty, setIsPasswordDirty] = useState(false)
+  const [repeatPassword, setRepeatPassword] = useState('')
+  const [isRepeatPasswordDirty, setIsRepeatPasswordDirty] = useState(false)
+  const navigate = useNavigate()
+
+  const handleClick = async () => {
+    try {
+      setIsLoading(true)
+      await axios.post('http://localhost:4000/register', {
+        email,
+        password,
+      })
+      navigate('/dashboard')
+    } catch (e) {
+      setIsAlertVisible(true)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -24,16 +54,29 @@ const Register = () => {
               <CCardBody className="p-4">
                 <CForm>
                   <h1>Register</h1>
+                  <CAlert
+                    color="primary"
+                    dismissible
+                    visible={isAlertVisible}
+                    onClose={() => setIsAlertVisible(false)}
+                  >
+                    Oops! There was an error processing your request.
+                  </CAlert>
                   <p className="text-medium-emphasis">Create your account</p>
                   <CInputGroup className="mb-3">
-                    <CInputGroupText>
-                      <CIcon icon={cilUser} />
-                    </CInputGroupText>
-                    <CFormInput placeholder="Username" autoComplete="username" />
-                  </CInputGroup>
-                  <CInputGroup className="mb-3">
                     <CInputGroupText>@</CInputGroupText>
-                    <CFormInput placeholder="Email" autoComplete="email" />
+                    <CFormInput
+                      placeholder="Email"
+                      autoComplete="email"
+                      value={email}
+                      onChange={(e) => {
+                        setEmail(e.target.value)
+                        setIsEmailDirty(true)
+                      }}
+                      required
+                      invalid={isEmailDirty && !validator.isEmail(email)}
+                    />
+                    <CFormFeedback invalid>Please provide a valid email.</CFormFeedback>
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
@@ -43,7 +86,18 @@ const Register = () => {
                       type="password"
                       placeholder="Password"
                       autoComplete="new-password"
+                      value={password}
+                      onChange={(e) => {
+                        setPassword(e.target.value)
+                        setIsPasswordDirty(true)
+                      }}
+                      required
+                      invalid={isPasswordDirty && !validator.isStrongPassword(password)}
                     />
+                    <CFormFeedback invalid>
+                      Password should have a minumum of 8 characters, 1 lowercase character, 1
+                      uppercase character, 1 number, and 1 symbol.
+                    </CFormFeedback>
                   </CInputGroup>
                   <CInputGroup className="mb-4">
                     <CInputGroupText>
@@ -53,10 +107,30 @@ const Register = () => {
                       type="password"
                       placeholder="Repeat password"
                       autoComplete="new-password"
+                      value={repeatPassword}
+                      onChange={(e) => {
+                        setRepeatPassword(e.target.value)
+                        setIsRepeatPasswordDirty(true)
+                      }}
+                      required
+                      invalid={isRepeatPasswordDirty && password !== repeatPassword}
                     />
+                    <CFormFeedback invalid>Passwords must match.</CFormFeedback>
                   </CInputGroup>
                   <div className="d-grid">
-                    <CButton color="success">Create Account</CButton>
+                    <CButton
+                      type="submit"
+                      color="success"
+                      onClick={() => handleClick()}
+                      disabled={
+                        isLoading ||
+                        !validator.isEmail(email) ||
+                        !validator.isStrongPassword(password) ||
+                        repeatPassword !== password
+                      }
+                    >
+                      Create Account
+                    </CButton>
                   </div>
                 </CForm>
               </CCardBody>
