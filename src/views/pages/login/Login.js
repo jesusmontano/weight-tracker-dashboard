9875d18c20b43f9ac261f7d988113793ec21c976
Jesus Montano <jesusmontano96@gmail.com>
2022-05-01 21:07:46 -0700

    feat: complete login page [WT-2]
    Integrates `/login` endpoint and adds minor validations.

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {
+  CAlert,
   CButton,
   CCard,
   CCardBody,
@@ -15,13 +16,43 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
+const axios = require('axios').default
 
 const Login = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isAlertVisible, setIsAlertVisible] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async () => {
+    try {
+      setIsLoading(true)
+      const { data } = await axios.post('http://localhost:4000/login', {
+        email,
+        password,
+      })
+      const { token } = data
+      // TODO: Set token in localStorage.
+    } catch (e) {
+      setIsAlertVisible(true)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
         <CRow className="justify-content-center">
           <CCol md={8}>
+            <CAlert
+              color="primary"
+              dismissible
+              visible={isAlertVisible}
+              onClose={() => setIsAlertVisible(false)}
+            >
+              Oops! There was an error processing your request.
+            </CAlert>
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
@@ -32,7 +63,14 @@ const Login = () => {
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
                       </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
+                      <CFormInput
+                        placeholder="Username"
+                        autoComplete="username"
+                        value={email}
+                        onChange={(e) => {
+                          setEmail(e.target.value)
+                        }}
+                      />
                     </CInputGroup>
                     <CInputGroup className="mb-4">
                       <CInputGroupText>
@@ -42,17 +80,21 @@ const Login = () => {
                         type="password"
                         placeholder="Password"
                         autoComplete="current-password"
+                        value={password}
+                        onChange={(e) => {
+                          setPassword(e.target.value)
+                        }}
                       />
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
+                        <CButton
+                          color="primary"
+                          className="px-4"
+                          disabled={isLoading}
+                          onClick={() => handleSubmit()}
+                        >
                           Login
-                        </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
                         </CButton>
                       </CCol>
                     </CRow>
@@ -63,10 +105,7 @@ const Login = () => {
                 <CCardBody className="text-center">
                   <div>
                     <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
+                    <p>Register to start tracking your progress.</p>
                     <Link to="/register">
                       <CButton color="primary" className="mt-3" active tabIndex={-1}>
                         Register Now!
